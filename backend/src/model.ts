fa7bfbccb334321c68b79ab67c6575dba767553d
ambesh333 <ambeshgaunker@gmail.com>
2025-02-23 10:49:05 +0530

    feat:fe

--- backend/src/model.ts
+++
@@ -1,8 +0,0 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import dotenv from "dotenv";
-dotenv.config();
-
-export const llm = new ChatGoogleGenerativeAI({
-  model: "gemini-2.0-flash-exp",
-  apiKey: process.env.GOOGLE_API_KEY,
-})
--- backend/src/utils/rpc.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-const rpcUrl = "https://rpc.blaze.soniclabs.com";
-const provider = new ethers.JsonRpcProvider(rpcUrl);
-
-export { provider };
